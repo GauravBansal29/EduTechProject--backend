185f9af5fa25a1c79758a50959b4f31ecc5e41e0
Gaurav Bansal <urrgb29@gmail.com>
2022-03-15 18:29:32 +0530

    Login Logout Complete

--- controller/auth.js
+++ controller/auth.js
@@ -63,7 +63,7 @@ export const login= async(req, res)=>{
 export const logout= async (req, res)=>{
     try{
     res.clearCookie("token");
-    res.status(200).json("Succesfully signed out");
+    return res.status(200).json("Succesfully signed out");
     }
     catch(err)
     {
