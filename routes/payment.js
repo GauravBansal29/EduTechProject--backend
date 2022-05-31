607313534148fe8558e9c975ff0a3b1b0ed32cf2
Gaurav Bansal <urrgb29@gmail.com>
2022-05-31 16:38:32 +0530

    Razorpay payments added

--- routes/payment.js
+++ routes/payment.js
@@ -1,12 +1,17 @@
 const router= require('express').Router();
 
 import {jwtSigned} from '../middlewares/index'
-import {addContact, addFundAccount} from '../controller/payment'
+import {addContact, addFundAccount, getRazorpayKey, createOrder, payOrder} from '../controller/payment'
 
 
 // controller 
+
+//making fund_account for payments to instructors
 router.post('/add-contact', jwtSigned, addContact);
 router.post('/add-fundaccount', jwtSigned, addFundAccount);
 
-
+//razorpay payment routes
+router.get('/get-razorpay-key', jwtSigned, getRazorpayKey);
+router.post('/create-order', jwtSigned, createOrder);
+router.post('/pay-order', jwtSigned, payOrder);
 module.exports= router;
