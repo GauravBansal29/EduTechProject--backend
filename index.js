635e532628413717aa88d754af22bbb1976a91df
Gaurav Bansal <urrgb29@gmail.com>
2023-02-06 09:32:28 +0530

    update

--- index.js
+++ index.js
@@ -39,20 +39,20 @@ app.use(cors({
     origin: "*",
     credentials: true
 }));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-    );
-    if ("OPTIONS" == req.method) {
-      res.send(200);
-    } else {
-      next();
-    }
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header("Access-Control-Allow-Origin", req.headers.origin);
+//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+//     );
+//     if ("OPTIONS" == req.method) {
+//       res.send(200);
+//     } else {
+//       next();
+//     }
+//   });
 //app.use(express.json());  
 app.use(morgan("dev")); // for status codes and errors 
 app.use(cookieParser());
